82c6b0b18e20abc4465342ec9d5512071c4f92d3
디지털토이즈 <pepsi2@gmail.com>
2020-11-22 16:20:22 +0900

    add weather ui

--- src/Clock.js
+++ src/Clock.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Clock() {
-  return <div>20:25:13</div>;
+  const [curTime, setCurTime] = useState("");
+
+  useEffect(() => {
+    setInterval(() => {
+      let now = new Date();
+      // console.log(now.toTimeString());
+      setCurTime(now.toTimeString().substr(0, 8));
+    }, 1000);
+  }, []);
+
+  return <div>{curTime}</div>;
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,9 @@
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // api.openweathermap.org/data/2.5/weather?q=seoul&appid=4c1f05438a742fac682c8cc9b31f9b6e
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import dummyData from "./dummyData";
 
 // const API_KEY = "60f2f5a42cdb41d0871131637202111";
 
@@ -20,15 +21,8 @@ import axios from "axios";
 //   return result;
 // }
 
-// function getFiveDayForecast(cityName) {
-//   const endpoint = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=5`;
-//   const result = fetch(endpoint)
-//     .then((response) => response.json())
-//     .then((data) => data);
-//   return result;
-// }
-
 export default function Weather() {
+  const [weatherData, setWeatherData] = useState(dummyData);
   useEffect(() => {
     // let a = getCurrentWeather("seoul")
     //   .then((res) => {
@@ -41,17 +35,32 @@ export default function Weather() {
     const fetchUsers = async () => {
       try {
         let seoul = await axios.get(
-          "/data/2.5/weather?q=seoul&appid=4c1f05438a742fac682c8cc9b31f9b6e",
-          // `http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108`,
+          // "/data/2.5/weather?q=seoul&appid=4c1f05438a742fac682c8cc9b31f9b6e",
+          "/v1/forecast.json?key=60f2f5a42cdb41d0871131637202111&q=seoul&days=1",
           {}
         );
-        console.log(seoul);
+        // console.log(seoul);
       } catch (e) {
         console.log(e);
       }
     };
     fetchUsers();
   }, []);
+  console.log(weatherData);
 
-  return <div>seoul</div>;
+  return (
+    <React.Fragment>
+      <div>{weatherData.location.name}</div>
+      <div className="d-flex justify-content-center ">
+        <img src={weatherData.current.condition.icon} />
+        <div className="temp">
+          <div className="font-m">{weatherData.current.temp_c} ℃</div>
+          <div className="font-sm">
+            {weatherData.forecast.forecastday[0].day.mintemp_c}/
+            {weatherData.forecast.forecastday[0].day.maxtemp_c}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
